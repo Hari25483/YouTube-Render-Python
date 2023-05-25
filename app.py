3c177fde3bf9d88202d35e2cfc2e807fbade5336
Hari25483 <44465072+Hari25483@users.noreply.github.com>
2023-05-25 22:18:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,request, jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/audio_feedback', methods=['GET', 'POST'])
+def get_audio_feedback():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return (jsonify(f.filename))
 
 if __name__ == '__main__':
     app.run()
